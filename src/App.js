12fdde1e0c4af7f475309207e3edcc624a78100f
dhwanitrranjan <dhwanit.ranjan@techmojo.in>
2023-09-18 20:38:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+function App() {
+  return (
+    <div className="h-screen w-full">
+      <NavBar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
